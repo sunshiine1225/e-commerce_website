cc9103554f671ad9914194fb4039b8bf8a120893
sunshiine_1225 <divyamalika325@gmail.com>
2022-06-05 20:51:40 +0530

    AMAZONA

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+       <a href="/">
+         amazona
+       </a>
+       <main>
+         list products
+       </main>
+      </header>
+    </div>
+  );
+}
+
+export default App;
